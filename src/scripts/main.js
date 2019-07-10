53ff81076d83f09baa7601b7d70f98d8e2eae8ee
Jake Scott <bemybanker@gmail.com>
2019-07-10 11:02:26 -0500

    initial comit

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,4 @@
 console.log("Your Webpack application is set up and ready to go. Please start writing code.")
+
+const test = document.querySelector("#container")
+test.innerHTML = "<h1>Is this Working</h1>"
